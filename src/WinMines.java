840b0a14897ea07f52b5e9bb9b2c382f44c6b3d6
Olivia <oyou@uwaterloo.ca>
2019-09-11 19:09:49 -0400

    updated comments

--- src/WinMines.java
+++ src/WinMines.java
@@ -1,6 +1,5 @@
 /**
  * Minesweeper
- * May 7th, 2018
  * Main program to define mine class
  **/
 
@@ -32,16 +31,15 @@ import javax.swing.border.LineBorder;
 
 public class WinMines extends JFrame implements ActionListener {
 	
-	 private Container pane; 
+	private static final long serialVersionUID = 1L;
+	private Container pane; 
 	 private JPanel board = new JPanel();; 
 	 final int CELL_SIZE = 30; 
 	
-	 // number of mines in each level
 	 final static int EASY_MINES_NUM = 10;
 	 final static int MEDIUM_MINES_NUM = 40;
 	 final static int HARD_MINES_NUM = 99;
 	
-	 // number of cells in each level
 	 final static int EASY_SIZE_ROW = 9;
 	 final static int EASY_SIZE_COL = 9;
 	 final static int MEDIUM_SIZE_ROW = 16;
@@ -54,22 +52,20 @@ public class WinMines extends JFrame implements ActionListener {
 	 static int SIZE_COL = EASY_SIZE_COL;
 	 static int MINES_NUM = EASY_MINES_NUM;
 	
-	 private JPanel minesBoard; // panel
-	 private final JLabel timer = new JLabel(""); // timer
+	 private JPanel minesBoard; 
+	 private final JLabel timer = new JLabel(""); 
 	 public static Mine[][] mines = new Mine[HARD_SIZE_ROW][HARD_SIZE_COL]; // max board size
 	 private int secondsLapsed; 
 	
-	 // possible states of the game
 	 public enum enumGameStatus {
 		 READY, STARTED, GAMEOVER, NONE, COMPLETED
 	 };
 	
 	 public static enumGameStatus gameStatus;
 	
-	 public static int minesOpened; // number of mines opened
-	 static JFrame frame = new JFrame("Minesweeper"); // main window 
+	 public static int minesOpened; 
+	 static JFrame frame = new JFrame("Minesweeper"); 
 	
-	 // global variable to store whether mine is clicked
 	 public static int mineOpenedRow;
 	 public static int mineOpenedCol;
 	
@@ -83,7 +79,6 @@ public class WinMines extends JFrame implements ActionListener {
 	 // Save board to serialized file
 	 public void saveBoard() {
 		  try {
-			   // Create the information to be serialized
 			   Mine.MineInfo[][] boardInfo = new Mine.MineInfo[SIZE_ROW][SIZE_COL];
 			   for (int i = 0; i < SIZE_ROW; i++) {
 				   for (int j = 0; j < SIZE_COL; j++) {
@@ -99,7 +94,6 @@ public class WinMines extends JFrame implements ActionListener {
 		  }
  	}
 	
-	 // Load board from serialized file
 	 public void loadBoard() {
 		 try {
 			 // Read serialized file
@@ -109,7 +103,7 @@ public class WinMines extends JFrame implements ActionListener {
 			 // Restore the current board with the serialized version loaded
 			 for (int i = 0; i < SIZE_ROW; i++) {
 				 for (int j = 0; j < SIZE_COL; j++) {
-					 mines[i][j].isExploded = boardInfo[i][j].isExploded;
+					 mines[i][j].hasExploded = boardInfo[i][j].hasExploded;
 				     mines[i][j].isMine = boardInfo[i][j].isMine;
 				     mines[i][j].minesNearBy = boardInfo[i][j].minesNearBy;
 				     mines[i][j].status = boardInfo[i][j].status;
@@ -121,7 +115,6 @@ public class WinMines extends JFrame implements ActionListener {
 		 }
 	 }
 	
-	 // initialize and create the mines
 	 public void createMines() {
 		  for (int row = 0; row < HARD_SIZE_ROW; row++) {
 			  for (int col = 0; col < HARD_SIZE_COL; col++) {
@@ -130,7 +123,6 @@ public class WinMines extends JFrame implements ActionListener {
 		  }
 	 }
 	
-	 // resets all mines when user starts the game
 	 public static void initMines() {
 		 for (int row = 0; row < HARD_SIZE_ROW; row++) {
 			 for (int col = 0; col < HARD_SIZE_COL; col++) {
@@ -139,35 +131,31 @@ public class WinMines extends JFrame implements ActionListener {
 		 }
 	 }
 	
-	 // randomly distribute mines 
-	 // passes row and column of the cell the user clicks
 	 public static void generateRandomMines(int r, int c) {
 		  int minesNum = 0;
 		  Random rand = new Random();
-		
 		  int counter = 0;
 		  
-		  // generates mines up to 10000 times to distribute mines and prevent infinite loop
+		  // generates mines up to 10000 times to prevent infinite loop
 		  while (minesNum < MINES_NUM && counter <= 10000) {
 			   int n = rand.nextInt(SIZE_ROW * SIZE_COL);
 			   int row = n / SIZE_COL;
 			   int col = n - row * SIZE_COL;
 			
-			   // ensure that user clicks cell that does not have a mine surrounding it to ensure first click opens an large area of cells
+			   // ensure first click opens an large area of cells
 			   if (row < r - 1 || row > r + 1 || col < c - 1 || col > c + 1) {
 				   if (mines[row][col].SetIsMine()) {
-					   // increment the mine count by one to achieve the desired number of mines according to level
 					   minesNum++;
 				   }
 			   }
-			   counter++; // prevent infinite loop
+			   counter++; 
 		  }
 		  
 		  if (minesNum != MINES_NUM) {
 			  JOptionPane.showMessageDialog(frame, "Error, only " + minesNum + " mines are generated.");
 		  }
 		
-		  // calculate nearby mines for all mines once the game begins go through each mine to calculate number displayed in cell
+		  // calculate number displayed in cell
 		  for (int row = 0; row < SIZE_ROW; row++) {
 			   for (int col = 0; col < SIZE_COL; col++) {
 				    int mineNum = 0;
@@ -192,23 +180,20 @@ public class WinMines extends JFrame implements ActionListener {
 			  }
 		 }
 	
-	 // restart timer after the round
 	 public void StartTimer() {
 		  gameStatus = enumGameStatus.STARTED;
 		  secondsLapsed = 0;
 	 }
 	
-	 // hide the timer when game finishes
 	 public void StopTimer() {
 		 timer.setText("");
 	 }
 	
-	 // timer to show time elapsed; automatically called every second
+	 // timer automatically called every second
 	 public void Timer() {
 		  if (gameStatus == enumGameStatus.STARTED) {
-			  secondsLapsed++; // increment timer by one every second
-			  // show time in minutes : seconds
-			  timer.setText(secondsLapsed / 60 + ":" + String.format("%02d", secondsLapsed % 60));
+			  secondsLapsed++;
+			  timer.setText(secondsLapsed / 60 + ":" + String.format("%02d", secondsLapsed % 60)); //minutes : seconds
 		  }
 	 }
 	
@@ -216,7 +201,7 @@ public class WinMines extends JFrame implements ActionListener {
 	 public void actionPerformed(ActionEvent event) {
 	 };
 	
-	 // checks if cell (the row and column) is located on board
+	 // checks if cell is on board
 	 public static boolean IsInBoard(int row, int col) {
 		  if (row < 0 || row >= SIZE_ROW || col < 0 || col >= SIZE_COL) {
 			  return false;
@@ -224,41 +209,34 @@ public class WinMines extends JFrame implements ActionListener {
 		  return true;
 	 }
 	
-	 // checks if cell is a mine
 	 public static boolean IsMine(int row, int col) {
-		  // ensure that cell (row and column) is in the board
 		  if (IsInBoard(row, col) == false) {
 			  return false;
 		  }
-		  // if cell exists, check if it's a mine
 		  return mines[row][col].GetIsMine();
 	 }
 	
-	 // add the mine gui to the board
+	 // add the mine GUI to the board
 	 public void AddMinesToBoard() {
-		  // create the panel
 		  minesBoard = new JPanel(new GridLayout(0, SIZE_COL));
 		
-		  minesBoard.setBorder(new LineBorder(Color.BLACK)); // set border of the panel to be black
-		  minesBoard.setPreferredSize(new Dimension(CELL_SIZE * SIZE_COL, CELL_SIZE * SIZE_ROW)); // adjust panel size to number of cells and size per cell
+		  minesBoard.setBorder(new LineBorder(Color.BLACK)); 
+		  minesBoard.setPreferredSize(new Dimension(CELL_SIZE * SIZE_COL, CELL_SIZE * SIZE_ROW));
 		
-		  board.add(minesBoard, BorderLayout.CENTER); // add board to the board
-		  pane.add(board, BorderLayout.CENTER); // add board to the content pane
+		  board.add(minesBoard, BorderLayout.CENTER); 
+		  pane.add(board, BorderLayout.CENTER); 
 		
-		  // add cell buttons to the board
 		  for (int row = 0; row < SIZE_ROW; row++) {
 			   for (int col = 0; col < SIZE_COL; col++) {
 				   minesBoard.add(mines[row][col].button);
 			   }
 		  }
 		
-		  // add mouse listener to detect clicking events
 		  minesBoard.addMouseListener(new MouseAdapter() {
 			   public void mousePressed(MouseEvent e) {
 				
 					 if (e.getButton() == MouseEvent.BUTTON1) { // left click
 					
-					     // start timer when game starts
 					     if (gameStatus == enumGameStatus.READY) {
 					    	 StartTimer();
 					     }
@@ -266,7 +244,6 @@ public class WinMines extends JFrame implements ActionListener {
 					     if (mineOpenedRow > -1 && mineOpenedCol > -1) {
 					    	 TryOpenEmptyMines(mineOpenedRow, mineOpenedCol);
 					     }
-					     // checks game status
 					     CheckGameStatus();
 				    }
 			   }
@@ -278,23 +255,22 @@ public class WinMines extends JFrame implements ActionListener {
 	
 	 // reset and initialize mines when new game starts
 	 public void NewGame() {
-		  gameStatus = enumGameStatus.READY; // reset status of the game to ready
-		  initMines(); // initialize/reset all mines
-		  board.removeAll(); // remove board elements
-		  AddMinesToBoard(); // reset and readd board elements
-		  minesOpened = 0; // reset number of opened mines to 0
-		  timer.setText(""); // hide time timer
+		  gameStatus = enumGameStatus.READY; 
+		  initMines(); 
+		  board.removeAll(); 
+		  AddMinesToBoard(); 
+		  minesOpened = 0; 
+		  timer.setText(""); 
 		  frame.repaint();
 		  frame.revalidate();
 	 }
 	
 	 // recursively open the mines when there are no adjacent mines
 	 public void TryOpenMine(int row, int col) {
-		  // check if cell is on the board
 		  if (IsInBoard(row, col) == false) {
 			  return;
 		  }
-		  // only try to open mine when it's not marked or opened
+		  // only try opening mine when it's not marked or opened
 		  if (mines[row][col].status == Mine.enumStatus.NONE) {
 			   mines[row][col].OpenMine();
 			   if (mines[row][col].GetMinesNearBy() == 0 && mines[row][col].status == Mine.enumStatus.OPEN) {
@@ -303,7 +279,7 @@ public class WinMines extends JFrame implements ActionListener {
 		  }
 	 }
 	
-	 // method to recursively find all the nearby cells that have no mines around it
+	 // recursively find all the nearby cells that have no mines around it
 	 public void TryOpenEmptyMines(int row, int col) {
 		  // when its not a mine and no adjacent mines, open all cells
 		  if (mines[row][col].GetIsMine() == false && mines[row][col].GetMinesNearBy() == 0) {
@@ -319,25 +295,18 @@ public class WinMines extends JFrame implements ActionListener {
 		  }
 	 }
 	
-	 // opens the cell
 	 private static void OpenMine(int row, int col) {
-		
-		  // check if cell is on the board
-		  // if not, no nothing
 		  if (IsInBoard(row, col) == false) {
 			  return;
 		  }
-		  // if it is on the board, open the cell
 		  mines[row][col].OpenMine();
 	 }
 	
 	 // checked is the row and column is marked
 	 public static boolean isMarked(int row, int col) {
-		  // check if cell is on the board
 		  if (IsInBoard(row, col) == false) {
 			  return false;
 		  }
-		  // check if mine is marked
 		  if (mines[row][col].status == Mine.enumStatus.MARKED) {
 			  return true;
 		  } else {
@@ -345,7 +314,6 @@ public class WinMines extends JFrame implements ActionListener {
 		  }
 	 }
 	
-	 // calculates the number of flagged cells nearby
 	 public static int getMarksNearby(int row, int col) {
 		  int num = 0;
 		  if (isMarked(row - 1, col - 1) == true) {
@@ -372,7 +340,6 @@ public class WinMines extends JFrame implements ActionListener {
 		  if (isMarked(row + 1, col + 1) == true) {
 			  num++;
 		  }
-		  // return the number of marked cells
 		  return num;
 	 }
 	
@@ -389,30 +356,24 @@ public class WinMines extends JFrame implements ActionListener {
 		  OpenMine(row + 1, col + 1);
 	 }
 	
-	 // checks the game status to output the status of the game
 	 public void CheckGameStatus() {
 		  if (gameStatus == enumGameStatus.STARTED || gameStatus == enumGameStatus.READY) {
-			   // start the game status to start the timer
 			   if (minesOpened > 0) {
 				   gameStatus = enumGameStatus.STARTED;
 			   }
-			   // when the game is completed, with all cells that are not mines opened, output a 'congratulations' message
 			   if (minesOpened == SIZE_ROW * SIZE_COL - MINES_NUM) {
 				    gameStatus = enumGameStatus.COMPLETED;
 				    JOptionPane.showMessageDialog(frame,
 				      "Congratulations, you win!"); // output message to the user
 			   }
 		  }
-		  // if user clicks on a mine, stop the timer and output a 'game over' message
 		  if (gameStatus == enumGameStatus.GAMEOVER) {
 			  StopTimer();
 			  JOptionPane.showMessageDialog(frame, "Game over, you hit the mine"); 
 		  }
 	 }
 	
-	 // redistributes mines across the board
 	 public void RedrawMines() {
-		  // go through each cell of the board
 		  for (int row = 0; row < SIZE_ROW; row++) {
 			   for (int col = 0; col < SIZE_COL; col++) {
 				   mines[row][col].Redraw();
@@ -420,23 +381,18 @@ public class WinMines extends JFrame implements ActionListener {
 		  }
 	 }
 	
-	 // add board to the centre of the pane
 	 public void addBoardToPane() {
 		  addMenuToBoard();
 		  pane.add(board, BorderLayout.CENTER);
 	 }
 	
-	 // add top bar to the board, including the levels and timer
 	 public void addMenuToBoard() {
 		  JToolBar tools = new JToolBar();
-		  pane.add(tools, BorderLayout.PAGE_START); // add tools and border to panel
+		  pane.add(tools, BorderLayout.PAGE_START); 
 		
-		  // add easy button to the panel
 		  final ButtonGroup group = new ButtonGroup();
 		  JToggleButton easyBtn = new JToggleButton("Easy"); 
-		  easyBtn.addActionListener(new ActionListener() { // add action listener
-			  
-			   // describe what to do when the easy button is clicked using an overriding method
+		  easyBtn.addActionListener(new ActionListener() {
 			   @Override
 			   public void actionPerformed(ActionEvent e) {
 				    SIZE_ROW = EASY_SIZE_ROW;
@@ -445,45 +401,38 @@ public class WinMines extends JFrame implements ActionListener {
 				    NewGame();
 			   }
 		  });
-		  group.add(easyBtn); // add easy button to the toggle group so only one button can be toggled
-		  easyBtn.setSelected(true); // set the easy button to be toggled
-		  tools.add(easyBtn); // add easy button to the toolbar
+		  group.add(easyBtn); // toggle group so only one button can be toggled
+		  easyBtn.setSelected(true); 
+		  tools.add(easyBtn); // add to toolbar
 		
-		  // define properties of a medium level game
 		  JToggleButton mediumBtn = new JToggleButton("Medium"); 
-		  mediumBtn.addActionListener(new ActionListener() { // add action listener
-			  
-		     // describe what to do when the medium button is clicked using an overriding method
+		  mediumBtn.addActionListener(new ActionListener() { 
 		     @Override
 		     public void actionPerformed(ActionEvent e) {
 			      SIZE_ROW = MEDIUM_SIZE_ROW;
 			      SIZE_COL = MEDIUM_SIZE_COL;
 			      MINES_NUM = MEDIUM_MINES_NUM;
-			      NewGame(); // reset game
+			      NewGame(); 
 			      }
 		     });
-		  	group.add(mediumBtn); // set the medium button to be toggled
-		  	tools.add(mediumBtn); // add medium button to the toolbar
+		  	group.add(mediumBtn); 
+		  	tools.add(mediumBtn); 
 		
-		  // define properties of a hard level game
 		  JToggleButton hardBtn = new JToggleButton("Hard"); 
 		  hardBtn.addActionListener(new ActionListener() { 
-			  
 		   @Override
 		   public void actionPerformed(ActionEvent e) {
 			    SIZE_ROW = HARD_SIZE_ROW;
 			    SIZE_COL = HARD_SIZE_COL;
 			    MINES_NUM = HARD_MINES_NUM;
-			    NewGame(); // reset game
+			    NewGame(); 
 			    }
 		  	});
-		  
-		  group.add(hardBtn); // set the hard button to be toggled
-		  tools.add(hardBtn); // add hard button to the toolbar
+		  group.add(hardBtn);
+		  tools.add(hardBtn); 
 		
 		  JToggleButton loadBtn = new JToggleButton("Load");
 		  loadBtn.addActionListener(new ActionListener() {
-			  
 			   @Override
 			   public void actionPerformed(ActionEvent e) {
 				    loadBoard();
@@ -491,9 +440,8 @@ public class WinMines extends JFrame implements ActionListener {
 				    frame.revalidate();
 			    }
 		  });
-		  
-		  group.add(loadBtn); // set the load button to be toggled
-		  tools.add(loadBtn); // add load button to the toolbar
+		  group.add(loadBtn); 
+		  tools.add(loadBtn); 
 		
 		  JToggleButton saveBtn = new JToggleButton("Save");
 		  saveBtn.addActionListener(new ActionListener() {
@@ -502,13 +450,12 @@ public class WinMines extends JFrame implements ActionListener {
 				  saveBoard();
 			  }
 		  });
+		  group.add(saveBtn);
+		  tools.add(saveBtn); 
 		
-		  group.add(saveBtn); // set the save button to be toggled
-		  tools.add(saveBtn); // add save button to the toolbar
-		
-		  tools.add(Box.createHorizontalGlue()); // add a horizontal tool bar
-		  tools.add(timer); // add timer to the tool bar
-		  tools.addSeparator(); // separate the level buttons from the timer
+		  tools.add(Box.createHorizontalGlue()); // horizontal tool bar
+		  tools.add(timer); 
+		  tools.addSeparator(); // separate level buttons from timer
 	 }
 	
 	 // define the application that runs the static instance of the WinMines class
@@ -520,31 +467,27 @@ public class WinMines extends JFrame implements ActionListener {
 		  
 		  app = new WinMines();
 		
-		  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // closes the app
-		  
-		  frame.setLocationByPlatform(true); // add the panel to the screen in cascading manner
+		  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		  frame.setLocationByPlatform(true);
 		
-		  app.pane = frame.getContentPane(); // get the contents of the pane
-		  app.addBoardToPane(); // add the board to the pane
+		  app.pane = frame.getContentPane(); 
+		  app.addBoardToPane(); 
 		
 		  frame.pack();
-		  frame.setSize(1024, 640); // set size of the panel
-		  frame.setVisible(true); // make the frame visible
+		  frame.setSize(1024, 640); 
+		  frame.setVisible(true); 
 		
-		  // create the timer task
 		  TimerTask repeatedTask = new TimerTask() {
 			   public void run() {
 				   app.Timer();
 			   }
 		  };
 		  
-		  // create timer
 		  java.util.Timer timer = new java.util.Timer("Timer");
 		  long delay = 1000L; // delay timer by one second for task execution
 		  long period = 1000L; // increment by one second every second
-		  timer.scheduleAtFixedRate(repeatedTask, delay, period); // set parameters as task for scheduling, delay and how often the timer is triggered
+		  timer.scheduleAtFixedRate(repeatedTask, delay, period); 
 	
-		  // start new game
 		  app.NewGame();
 	 }
 }
